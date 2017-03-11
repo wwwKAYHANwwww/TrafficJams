abd02b3a0ad434a87d0f9b52c842835ff259df12
wwwKAYHANwwww <Kayhan.Momeni1995@gmail.com>
2017-03-11 22:00:25 +0330

    Test of Editing

--- Model/Car.py
+++ Model/Car.py
@@ -1,14 +1,14 @@
 from ITimeDependent import ITimeDependent
 
 class Car(ITimeDependent):
-    def __init__(self,v0,x0,targetVelocity,name,frontCar):
-        self.v0=v0
-        self.x0=x0
-        self.x=0
-        self.v=0
-        self.targetVelocity= targetVelocity
-        self.frontCar=frontCar;
-        self.acceleration=0
+    def __init__(self,v0,x0,targetVelocity,name,frontCar): #Constractor of the class
+        self.v0=v0                                         #Initial Velocity
+        self.x0=x0                                         #Initial Position
+        self.x=0                                           #Starting Point     ???
+        self.v=0                                           #Statrting Velocity ???
+        self.targetVelocity= targetVelocity                #TargetVelocity
+        self.frontCar=frontCar;                            #The Car in Front of me (Should be Updated Once)
+        self.acceleration=0                                #
         self.name=name
         self.positiveA=0.5
         self.negativeA=-1.
