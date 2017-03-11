cd23bdff8ffd411e24c5d67c35187b621a8233b3
Amin <Amin@hotmail.com>
2017-03-11 15:36:05 +0330

    Adding Constrcutor to Car Class

---
+++ Model/Car.py
@@ -0,0 +1,8 @@
+class Car(object):
+    def __init__(self,v0,x0,targetVelocity,frontCar):
+        self.v0=v0
+        self.x0=x0
+        self.targetVelocity= targetVelocity
+        self.fronCar=frontCar
+        
+    
