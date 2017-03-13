e57e28710b3c4356026a178dc777600ee4f2f185
Amin <Amin@hotmail.com>
2017-03-13 16:24:27 +0330

    Modifying initialize in DataFileLogger

--- Logger/DataFileCarLogger.py
+++ Logger/DataFileCarLogger.py
@@ -1,10 +1,11 @@
 from ICarLogger import ICarLogger
 
 class DataFileCarLogger(ICarLogger):
-    def __init__(self):
+    def __init__(self,car=None):
         self.car=car
         
     def initialize(self,car):
+        self.car=car
         self.file=open("report/{}.dat".format(car.name),"a")
         
         
