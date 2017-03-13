82ea4aea419958544b499754eab9db4fcf025349
wwwKAYHANwwww <Kayhan.Momeni1995@gmail.com>
2017-03-13 17:19:28 +0330

    Some minor bugs fixed!

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ CONST_MinimumDistance = 10
 CONST_MaxNoise = 1
 CONST_LengthOfRoad=1000
 
+clock=Clock(.1, 20)
 cars=[]
 position = 0.
 fileLogger=DataFileCarLogger()
 tmpCar1=Car(0,position,"car1", maxVelocity=20, logger=fileLogger)
 fileLogger.initialize(tmpCar1)
+clock.addListener(tmpCar1)
 cars.append(tmpCar1)
-
-
-clock=Clock(.1, 20)
 
 position += CONST_MinimumDistance + random.uniform(0,CONST_MaxNoise)
 for i in range(2,10):
@@ -32,10 +31,5 @@ cars[0].frontCar=cars[-1]
 
 for i in cars:
     print(i.name,i.frontCar.name,i.x )
-    
 
-def updateFrontCars():                              #Should be defined
-    pass
-
-updateFrontCars()
 clock.start()
