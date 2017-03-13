5b5610ca2df65548ab47949eefec0d7d72ee4814
Amin <Amin@hotmail.com>
2017-03-13 16:19:53 +0330

    Modifying Dafile Logger

--- Logger/DataFileCarLogger.py
+++ Logger/DataFileCarLogger.py
@@ -1,9 +1,12 @@
 from ICarLogger import ICarLogger
 
 class DataFileCarLogger(ICarLogger):
-    def __init__(self,car):
+    def __init__(self):
         self.car=car
+        
+    def initialize(self,car):
         self.file=open("report/{}.dat".format(car.name),"a")
+        
         
     def log(self,time):
         file.write("{}     {}\n".format(time,self.car.x))
