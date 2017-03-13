78831fa81e4d70ced2222b7180d1b070e4fd52b0
wwwKAYHANwwww <Kayhan.Momeni1995@gmail.com>
2017-03-13 16:43:35 +0330

    Modifying Car Factory

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 from Car import Car
 from Clock import Clock
+import random
+from Logger.DataFileCarLogger import DataFileCarLogger
 
-Const_MinimumDistance = 30
+CONST_MinimumDistance = 10
+CONST_MaxNoise = 1
+CONST_LengthOfRoad=1000
+
 cars=[]
 tmpCar1=Car(10,15,20,"car1")
 cars.append(tmpCar1)
 
+
 clock=Clock(.1, 20)
 
+position = 0.0
 for i in range(2,10):
-    tmpCar2=Car(10,15,"car{}".format(i),maxVelocity=20)
+    fileLogger=DataFileCarLogger()
+    tmpCar2=Car(0,position,"car{}".format(i),maxVelocity=20, logger=fileLogger)
+    fileLogger.initialize(tmpCar2)
     tmpCar2.setFrontCar(tmpCar1)
     cars.append(tmpCar2)
     clock.addListener(tmpCar2)
     tmpCar1=tmpCar2
+    position += CONST_MinimumDistance + random.uniform(0,CONST_MaxNoise)
     
 cars[0].frontCar=cars[-1]
 
 for i in cars:
-    print(i.name,i.frontCar.name)
+    print(i.name,i.frontCar.name,i.x )
+    
 
 def updateFrontCars():                              #Should be defined
     pass
