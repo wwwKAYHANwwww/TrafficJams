46661e962174d91e5336e334d713d0b9b8497823
Amin <Amin@hotmail.com>
2017-03-14 12:57:26 +0330

    Adding getRandomMaxVelocity and getRanfomPosition to CarFactory

--- Factory/CarFactory.py
+++ Factory/CarFactory.py
@@ -18,25 +18,29 @@ class CarFactory(object):
     def startBuilding(self):
         fileLogger=self.loggerFactory.getLogger()
         position=0.0
-        tmpCar1=Car(0,position,"car1", maxVelocity=25+random.uniform(-Config.CONST_MaxVelocityNoise, Config.CONST_MaxVelocityNoise), logger=fileLogger)
+        tmpCar1=Car(0,position,"car1", maxVelocity=self.getRandomMaxVelocity(25), logger=fileLogger)
         fileLogger.initialize(tmpCar1)
         self.clock.addListener(tmpCar1)
         self.cars.append(tmpCar1)
         
-        position += Config.CONST_MinimumDistance*2 + random.uniform(0,Config.CONST_MaxPositionNoise)
+        position =self.getRanfomPosition(position) 
         for i in range(2,10):
             fileLogger=self.loggerFactory.getLogger()
-            tmpCar2=Car(0,position,"car{}".format(i),maxVelocity=20+random.uniform(-Config.CONST_MaxVelocityNoise, Config.CONST_MaxVelocityNoise), logger=fileLogger)
+            tmpCar2=Car(0,position,"car{}".format(i),maxVelocity=self.getRandomMaxVelocity(25), logger=fileLogger)
             fileLogger.initialize(tmpCar2)
             tmpCar1.setFrontCar(tmpCar2)
             self.cars.append(tmpCar2)
             self.clock.addListener(tmpCar2)
             tmpCar1=tmpCar2
-            position += Config.CONST_MinimumDistance*2 + random.uniform(0,Config.CONST_MaxPositionNoise)
+            position =self.getRanfomPosition(position) 
             
         self.cars[-1].frontCar=self.cars[0]
         for i in self.cars:
             print(i.name,i.frontCar.name,i.x )
 
 
+    def getRandomMaxVelocity(self,mean):
+        return mean+random.uniform(-Config.CONST_MaxVelocityNoise, Config.CONST_MaxVelocityNoise)
         
+    def getRanfomPosition(self,previousPosition):
+        return previousPosition+Config.CONST_MinimumDistance*2 + random.uniform(0,Config.CONST_MaxPositionNoise)
