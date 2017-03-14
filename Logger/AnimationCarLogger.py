68cdb5da9b50d029fc1620a9d7fdf6e80141e07c
Amin <Amin@hotmail.com>
2017-03-14 18:55:00 +0330

    Creating AnimationCarLogger

---
+++ Logger/AnimationCarLogger.py
@@ -0,0 +1,21 @@
+from ICarLogger import ICarLogger
+
+class AnimationCarLogger(ICarLogger):
+    def __init__(self,cars=None):
+        self.cars=cars
+        self.file=[]
+        
+    def initialize(self,cars):
+        self.cars=cars
+        self.file=open("report/animation.dat","w")
+        
+    def update(self,time,timeStep):
+       
+        for i in range(len(self.cars)):
+            self.file.write("{}    {}    {}    {}\n".format(i,self.cars[i].x,self.cars[i].v,time))
+            
+        self.file.write("\n\n")
+    def finalize(self):
+        
+        self.file[i].close()
+    
