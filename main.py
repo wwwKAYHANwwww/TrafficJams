f355a3601943b457e8a468815a89cdbfb6dfd636
wwwKAYHANwwww <Kayhan.Momeni1995@gmail.com>
2017-03-14 12:50:53 +0330

    Simulation Time Increased

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Factory.CarFactory import CarFactory
 from Factory.DataFileCarLoggerFactory import DataFileCarLoggerFactory
 
 
-clock=Clock(.01, 100)
+clock=Clock(.01, 1000)
 fileLoggerFactory=DataFileCarLoggerFactory()
 carFactory=CarFactory(10,fileLoggerFactory,clock)
 carFactory.startBuilding()
